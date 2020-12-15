b02cd142ba8e37cb55cb05358f0a4e367eb6111b
elliott <elliottdury06@gmail.com>
2020-12-15 16:13:16 +0100

    upload2

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ document.forms["textarea"]["text"].value = localStorage.getItem("text");
 function remove() {
 document.forms["textarea"]["text"].value = "";
 }
+
+function copy() {
+  var copy = document.getElementById("txtarea");
+
+  copy.select();
+  copy.setSelectionRange(0, 99999);
+
+  document.execCommand("copy");
+
+  window.alert("Copied!");
+}
+
+function onload() {
+
+document.getElementById("txtarea").style.color = "black";
+document.forms["textarea"]["text"].value = "start typing here..";
+}
+
+function about() {
+window.alert("coming soon..");
+}
+
+function customize() {
+window.alert("coming soon..")
+}
